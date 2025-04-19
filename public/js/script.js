103b1265ab776f8b7e59126c5552b15febe2e9e1
ZyleStacion <s4064846@rmit.edu.vn>
2025-04-20 02:43:35 +0700

    open and close hamburger added

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,10 @@
 function openHamburger() {
     document.getElementById('navbar').style.visibility = 'visible';
+    document.getElementById('close').style.visibility = 'visible';
+    document.getElementById('hamburger').style.visibility = 'hidden';
+}
+function closeHamburger() {
+    document.getElementById('navbar').style.visibility = 'hidden';
+    document.getElementById('close').style.visibility = 'hidden';
+    document.getElementById('hamburger').style.visibility = 'visible';
 }
