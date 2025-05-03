323a6662a071813e0b0368d01bfb05720e8fbe33
ZyleStacion <s4064846@rmit.edu.vn>
2025-05-04 03:27:33 +0700

    decided not to use anchors due to lack of production support

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,5 @@
-function openHamburger() {
-    document.getElementById('navbar').style.visibility = 'visible';
-    document.getElementById('close').style.visibility = 'visible';
-    document.getElementById('hamburger').style.visibility = 'hidden';
-}
-function closeHamburger() {
-    document.getElementById('navbar').style.visibility = 'hidden';
-    document.getElementById('close').style.visibility = 'hidden';
-    document.getElementById('hamburger').style.visibility = 'visible';
+function toggleHamburgerMenu() {
+    document.getElementById('hamburger').classList.toggle('hidden');
+    document.getElementById('close').classList.toggle('hidden');
+    document.getElementById('navbar').classList.toggle('visible');
 }
