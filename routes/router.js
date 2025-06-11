7ad4987b45aae187f09b962497f865dadca1091c
ZyleStacion <s4064846@rmit.edu.vn>
2025-06-11 13:26:22 +0700

    basic bookshelf css, removed logic for active page

--- routes/router.js
+++ routes/router.js
@@ -6,11 +6,15 @@ const { getBooks } = require('../models/book')
 
 router.get("/", (req, res) => {
     const books = getBooks()
-    res.render("index", { books });
+    res.render("index", { books, title: "Home" });
 });
 
 router.get("/reviews", function (req, res) {
     res.send("Review Page")
+})
+
+router.get("/search", function (req, res) {
+    res.render("search", {title: "Search"})
 })
 
 module.exports = router;
