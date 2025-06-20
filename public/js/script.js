a53a4f1fcca20de7a28412162edc4b45211ccb16
ZyleStacion <s4064846@rmit.edu.vn>
2025-06-20 18:47:28 +0700

    added templating function for displaying search results

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,39 @@
 console.log("JS is connected.")
 
+function createBookCard(book) {
+    // Display the book cards in HTML
+    let bookCard = document.createElement("div");
+    bookCard.className = "bookCard";
+    searchResults.appendChild(bookCard);
+
+    // Cover
+    let bookCover = document.createElement("img");
+    bookCard.appendChild(bookCover);
+    bookCover.src = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`;
+    bookCover.alt = `${book.title} cover`;
+
+    // Title
+    let bookTitle = document.createElement("p");
+    bookTitle.innerHTML = book.title;
+    bookTitle.class = "bookTitle";
+    bookCard.appendChild(bookTitle);
+
+    // Might want to show up to 3 authors, join each with a comma
+    let bookAuthor = document.createElement("p");
+    // Undefined?
+    bookAuthor.innerHTML = book.author;
+    bookAuthor.class = "bookAuthor";
+    bookCard.appendChild(bookAuthor);
+
+    // Publish year
+    let bookYear = document.createElement("p");
+    bookYear.innerHTML = book.first_publish_year;
+    bookYear.class = "bookYear";
+    bookCard.appendChild(bookYear);
+
+    return bookCard;
+};
+
 window.addEventListener("DOMContentLoaded", function() {
     // Get user search
     const search = document.getElementById('query');
@@ -24,33 +58,14 @@ window.addEventListener("DOMContentLoaded", function() {
 
                 const data = await response.json();
                 const docs = data.docs;
-
-                // not entering this loop
-                console.log(docs);
+                searchResults.classList.toggle('visible');
+                // Display each book
                 docs.forEach(book => {
-                    console.log('running')
-                    // Display the book cards in HTML
-                    let bookCard = document.createElement("div");
+                    console.log(book)
+
+                    // Templating function
+                    const bookCard = createBookCard(book);
                     searchResults.appendChild(bookCard);
-
-                    // Cover
-                    let bookCover = document.createElement("img");
-                    bookCard.appendChild(bookCover);
-                    bookCover.src = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`;
-
-                    // Title
-                    let bookTitle = document.createTextNode(book.title);
-                    bookTitle.class = "bookTitle"
-                    bookCard.appendChild(bookTitle);
-
-                    // Might want to show up to 3 authors, join each with a comma
-                    let bookAuthor = document.createTextNode(book.author_name[0]);
-                    bookAuthor.class = "bookAuthor";
-                    bookCard.appendChild(bookAuthor);
-
-                    let bookYear = document.createTextNode(book.first_publish_year);
-                    bookYear.class = "bookYear";
-                    bookCard.appendChild(bookYear);
                 });
             }
             catch(error) {
