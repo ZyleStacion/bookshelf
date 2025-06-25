701522a4a1a4b675b5298c78fc584d68ea999bfb
ZyleStacion <s4064846@rmit.edu.vn>
2025-06-25 18:15:28 +0700

    please fix SEARCH for TITLES only

--- public/js/script.js
+++ public/js/script.js
@@ -44,9 +44,20 @@ window.addEventListener("DOMContentLoaded", function() {
         
         // Fetch the search query, need to encode URI
         const fixedSearch = encodeURI(query.target.value);
+
+        // BUG: not returning correct search results.
+            // TODO: Specify search query to EXCLUSIVELY book titles
         const searchURL = `https://openlibrary.org/search.json?q=${fixedSearch}&limit=5`;
 
+        // Clear searchResult child nodes (bookCards)
+        if (searchResults.hasChildNodes()) {
+            while (searchResults.firstChild) {
+                searchResults.removeChild(searchResults.firstChild);
+            }
+        }
+        
         getResults()
+        
         // Stagger getResults() to wait for input to complete
         async function getResults() {
             try {
@@ -61,16 +72,7 @@ window.addEventListener("DOMContentLoaded", function() {
 
                 const data = await response.json();
                 const docs = data.docs;
-                searchResults.classList.toggle('visible');
-                const bookCardElement = document.getElementById("bookCard");
 
-                // Not entering this branch
-                // Clear book results in DOM
-                if (bookCardElement) {
-                    // Delete bookCards
-                    bookCardElement.remove();
-                    console.log("removed");
-                }
                 // Display each book
                 docs.forEach(book => {
                     console.log(book);
@@ -87,82 +89,8 @@ window.addEventListener("DOMContentLoaded", function() {
     })
 })
 
-//         // response returns a Promise(<pending>)
-//         const response = fetch(searchURL);
-        
-
-//         // All info we need is in 'docs'
-//         try {
-//             let docs = data.docs;
-
-//             for (book of docs) {
-//                 // Display the book cards in HTML
-//                 let bookCard = document.createElement("div");
-//                 searchResults.appendChild(bookCard);
-
-//                 // How to get the book cover? It needs another API call
-//                 let bookCover = document.createElement("img");
-//                 bookCard.appendChild(bookCover);
-//                 bookCover.src = `https://covers.openlibrary.org/b/id${book.cover_i}-M.jpg`;
-
-//                 let bookTitle = document.createTextNode(book.title);
-//                 // Change styling with css
-//                 bookTitle.class = "bookTitle"
-//                 bookCard.appendChild(bookTitle);
-
-//                 // Might want to show up to 3 authors, join each with a comma
-//                 let bookAuthor = document.createTextNode(book.author_name[0]);
-//                 bookAuthor.class = "bookAuthor";
-//                 bookCard.appendChild(bookAuthor);
-
-//                 let bookYear = document.createTextNode(book.first_publish_year);
-//                 bookYear.class = "bookYear";
-//                 bookCard.appendChild(bookYear);
-//             }
-//         }
-//         catch(err) {
-//             console.log("Invalid search: ", err);
-//         }
-//     })
-// })
-
 function toggleHamburgerMenu() {
     document.getElementById('hamburger').classList.toggle('hidden');
     document.getElementById('close').classList.toggle('hidden');
     document.getElementById('navbar').classList.toggle('visible');
 }
-
-// // TODO: Display books in HTML
-// function displaysearchResults(book) {
-//     console.log("Started")
-//     const resultsPane = document.getElementById("searchResults");
-//     resultsPane.classList.toggle('visible')
-
-//     // Create HTML card elements for each book
-//     const bookCard = document.createElement('div');
-//     bookCard.className = "bookCard";
-
-//     const bookCover = document.createElement('img');
-//     // Call getBookCover function?
-
-//     const bookTitle = document.createElement('p');
-//     bookTitle.className = "bookTitle";
-//     bookTitle.textContent = book.Title;
-
-//     const bookAuthor = document.createElement('p');
-//     bookAuthor.className = "bookAuthor";
-//     bookAuthor.textContent = book.Author;
-
-//     const bookYear = document.createElement('p');
-//     bookYear.className = "bookYear";
-//     bookYear.textContent = book.Year;
-
-//     bookCard.appendChild(bookCover);
-//     bookCard.appendChild(bookTitle);
-//     bookCard.appendChild(bookAuthor);
-//     bookCard.appendChild(bookYear);
-
-//     resultsPane.append(bookCard)
-
-//     console.log("ended")
-// }
